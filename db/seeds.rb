708944ffa2de2387820ec36b8a3baecea824c355
Wan Cheul Kim <wckim88@gmail.com>
2018-01-04 19:28:00 -0800

    Assignment3_16: Seed unique post and unique comment without duplicate

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,12 @@ require 'random_data'
     body:  RandomData.random_paragraph
   )
 end
+
+Post.find_or_create_by!(
+  title: "This is unique title!",
+  body: "This is unique body!"
+)
+
 posts = Post.all
 
 # Create Comments
@@ -16,6 +22,11 @@ posts = Post.all
     body: RandomData.random_paragraph
   )
 end
+
+Comment.find_or_create_by!(
+  post: posts.find_by(title: "This is unique title!"),
+  body: "This is unique comment body!"
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
