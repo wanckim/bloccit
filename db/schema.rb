7778985d6b1f85973b40080f36687f87713e897c
Wan Cheul Kim <wckim88@gmail.com>
2018-01-18 00:04:51 -0800

    Assignment3_21: Sponsored Post added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117022409) do
+ActiveRecord::Schema.define(version: 20180118020246) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -51,6 +51,16 @@ ActiveRecord::Schema.define(version: 20180117022409) do
     t.boolean "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sponsored_posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "price"
+    t.integer "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
   end
 
   create_table "topics", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -39,9 +39,20 @@ end
 #   body: "This is unique comment body!"
 # )
 
+# Create SponsoredPosts
+50.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: RandomData.random_price
+  )
+end
+
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
 
 
