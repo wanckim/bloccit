55b0757f507cf480517df3132b105ea0241c0c03
Wan Cheul Kim <wckim88@gmail.com>
2018-01-23 11:01:11 -0800

    Assignment3_25: User avatar added

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,9 @@ module SessionsHelper
   def current_user
     User.find_by(id: session[:user_id])
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=64"
+  end
 end
