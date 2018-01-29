ae5e5d189ffffb0ef8272ecfba772165cd0c25da
Wan Cheul Kim <wckim88@gmail.com>
2018-01-29 14:01:38 -0800

    Checkpoint3_29: Voting model added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
+    post '/up_vote' => 'votes#up_vote', as: :up_vote
+    post '/down_vote' => 'votes#down_vote', as: :down_vote
   end
 
   resources :users, only: [:new, :create]
